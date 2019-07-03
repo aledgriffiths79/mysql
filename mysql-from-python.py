b72eb10f4172676da99e840646934b245332e7e3
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-07-03 13:44:53 +0100

    adding sql insert command

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -16,12 +16,10 @@ db="chinook")
 try:
   # run a query
   with connection.cursor() as cursor:
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS
-                    Friends(name char(20), age int, DOB datetime);""")
-    # Note that the above will still display a warning (not error) if the table already exists
-    for row in cursor:
-      print(row)
-
+    row = ("Bob", 21, "1990-02-06 23:04:56")
+    cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+    connection.commit()
+    
 finally:
   # close the connection, regardless of whether the above was successful
   connection.close()
